cb56a2d4e0f61d14b3bfd29d45163e6a5bc72778
Nikolay Kulikov <nikolayof23@gmail.com>
2026-03-13 15:09:24 +0300

    lib: keyboards: add internal func to refresh button

--- main/src/lib/keyboards_lib.cpp
+++ main/src/lib/keyboards_lib.cpp
@@ -8,5 +8,38 @@
 uint8_t keyboards_lib_init(struct keyboard_service *service)
 {}
 
-uint8_t keyboards_lib_refresh(struct keyboard_service *service)
+
+uint8_t _refresh_encoder(struct encoder *enc)
 {}
+
+
+uint8_t _refresh_button(struct button *btn)
+{
+	struct keyboard_action *action;
+
+	btn->obj->tick();
+
+	if (btn->obj->click()) {
+		if (!btn->click || !btn->click->func)
+			return 1;
+
+		action = btn->click;
+		action->func(action->data);
+		return 0;
+	}
+
+	if (btn->obj->hold()) {
+		if (!btn->long_click || !btn->long_click->func)
+			return 1;
+
+		action = btn->long_click;
+		action->func(action->data);
+		return 0;
+	}
+
+	return 0;
+}
+
+uint8_t keyboards_lib_refresh(struct keyboard_service *service)
+{
+}
